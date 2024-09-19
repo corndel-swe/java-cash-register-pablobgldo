12d98c56fd6ca29dd5c81ee7fbb97f461a202cd8
Simon Haines <nimon@hotmail.co.uk>
2024-09-19 15:28:59 +0100

    Update Exercise2a.java

--- src/main/java/com/corndel/cashregister/exercises/Exercise2a.java
+++ src/main/java/com/corndel/cashregister/exercises/Exercise2a.java
@@ -8,14 +8,7 @@ public class Exercise2a {
    * Given a list of recipes, return the number of vegetarian recipes in the list.
    */
   public static int countVeggies(List<Recipe> recipes) {
-    var count = 0;
-
-    for (var recipe : recipes) {
-      if (recipe.isVegetarian) {
-        count++;
-      }
-    }
-
-    return count;
+    // TODO
+    return 0;
   }
 }
