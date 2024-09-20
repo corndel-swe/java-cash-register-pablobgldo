962c774a05d6e58b67378849cd9615ecffef447e
pablobgldo <pablobgldo@gmail.com>
2024-09-20 15:19:29 +0100

    Completed Levels 1, 2, 3 and 4

--- src/main/java/com/corndel/cashregister/Level3.java
+++ src/main/java/com/corndel/cashregister/Level3.java
@@ -8,7 +8,6 @@ public class Level3 {
    * Calculates the total value of all money in the drawer.
    */
   public static int sumDrawer(List<Item> drawer) {
-    // TODO
-    return 0;
+    return drawer.stream().map(x->x.quantity*x.value).reduce(0, Integer::sum);
   }
 }
